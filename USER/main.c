c7d6a0f84a04cf439d673cfb55198e0f2ef77c96
shenyi-q <shenyiq@umich.edu>
2023-04-20 01:49:28 -0400

    add functions

--- USER/main.c
+++ USER/main.c
@@ -22,6 +22,8 @@ u16 t;
 u16 len;
 u8 runover = 0;
 
+
+
 extern u8 step1;
 extern u8 step2;
 extern u8 step3;
@@ -40,6 +42,10 @@ int acceleration = 100; // Accelration to use.
 int deceleration = 100;  // Deceleration to use.
 int speed = 800; // Speed to use.
 char okCmd = FALSE; // Tells if the received string was a valid command.
+const u16 step;
+int emb_dir = 1;
+int emb_pos = 1;
+
 //****************************************************
 void initFunction()		 //各个功能初始化
 {
@@ -89,6 +95,117 @@ void runOver()
     }
 }
 
+//pos: 0 for left, 1 for middle, 2 for right
+//dir: 1 for left, 0 for right
+void Calibrate(){
+		char prev_pos = message[2];
+	  char prev_dir = message[1];
+		
+		if(prev_pos == '0'){
+			Dir = 0; //right turn
+		}
+		else if(prev_pos == '2'){
+			Dir = 1; //left turn			
+		}
+		else if(prev_pos == '1'){
+			if(bigStepMotorSwitch_State_PA0 == 1){//not on the middle switch
+					if(prev_dir == '1') Dir = 0;
+					else Dir = 1;
+			}
+			else{//on the middle or other switches, do nothing
+					okCmd = TRUE;
+					printf("runover\r\n");
+					return;
+			}
+		}
+		else{
+			return;
+		}
+		
+		runover = 0;
+    speed_cntr_Move(step, 20, 20, 2000);
+
+
+    okCmd = TRUE;
+    runOver();
+		emb_dir = 1;
+		emb_pos = 1;
+}
+
+void Manual_cal(){
+	
+	printf("manual\r\n");	
+	memset(message, 0, sizeof message);                          //清空数组
+  USART_RX_STA = 0;
+	
+	while(1){
+			if(USART_RX_STA & 0x8000)
+    {
+        len = USART_RX_STA & 0x3fff; //得到此次接收到的数据长度
+        //printf("\r\n您发送的消息为:\r\n\r\n");
+
+        for(t = 0; t < len; t++)
+        {
+            sprintf(&message[t], "%c", USART_RX_BUF[t]);
+        }
+				
+				if(strcmp(message, "Start") == 0) {
+										
+					printf("ready\r\n");
+					return;
+				}
+				
+				if(message[0] == 'm')		//r为旋转大电机，r0向右转，r1向左转
+        {
+            u16 value = 0;
+            u8 message_length =  strlen(message);
+            if(message_length == 3)
+            {
+                value = message[2] - '0';
+
+            }
+            else if(message_length == 4)
+            {
+                value = (message[2] - '0') * 10 + (message[3] - '0');
+            }
+            else if(message_length == 5)
+            {
+                value = (message[2] - '0') * 100 + (message[3] - '0') * 10 + (message[4] - '0');
+            }
+            Dir = message[1] - '0';
+            //如果已经处于零位，且还是向左转，就不要旋转了，代表已经处于零位。
+//            if(bigStepMotorSwitch_State_PA0 == 0)
+////							|| bigStepMotorSwitch_State_PA1 == 0 || bigStepMotorSwitch_State_PA4 == 0)
+////							&& (Dir == 1))
+//            {
+//                runover = 1;
+//            }
+//            else
+//            {
+            runover = 0;
+            speed_cntr_Move(value, 20, 20, 2000);
+//            }
+
+            okCmd = TRUE;
+            runOver();
+        }
+				
+			  memset(message, 0, sizeof message);                          //清空数组
+        USART_RX_STA = 0;
+				
+			}
+		
+		
+	}
+	
+	
+				
+				
+	
+	
+}
+
+
 
 void runZeroWhileForWitch()
 {
@@ -125,40 +242,50 @@ void usartReceiveMessage()
         {
             sprintf(&message[t], "%c", USART_RX_BUF[t]);
         }
-        if(message[0] == 'r')		//r为旋转大电机，r0向右转，r1向左转
-        {
-            u16 value = 0;
-            u8 message_length =  strlen(message);
-            if(message_length == 3)
-            {
-                value = message[2] - '0';
+        
+				if(message[0] == 'c'){
+					Calibrate();
+				}
+				
+				if(strcmp(message, "Manual") == 0){
+					Manual_cal();					
+				}
+				
+				if(message[0] == 'r'){
+					int pc_pos = message[2]-'0';
+					int pc_dir = message[1]-'0';
+					if(pc_pos != emb_pos || pc_dir != emb_dir || bigStepMotorSwitch_State_PA0 == 1){
+						printf("error\r\n");
+					}
+					if(emb_pos == 1 && emb_dir == 1){
+						Dir = 1;
+						emb_pos = 0;
+						emb_dir = 0;
+					}
+					else if(emb_pos == 0 && emb_dir == 0){
+						Dir = 0;
+						emb_pos = 1;
+						emb_dir = 0;
+					}
+					else if(emb_pos == 1 && emb_dir == 0){
+						Dir = 0;
+						emb_pos = 2;
+						emb_dir = 1;
+					}
+					else if(emb_pos == 2 && emb_dir == 1){
+						Dir = 1;
+						emb_pos = 1;
+						emb_dir = 1;
+					}
+					runover = 0;
+          speed_cntr_Move(step, 20, 20, 2000);
 
-            }
-            else if(message_length == 4)
-            {
-                value = (message[2] - '0') * 10 + (message[3] - '0');
-            }
-            else if(message_length == 5)
-            {
-                value = (message[2] - '0') * 100 + (message[3] - '0') * 10 + (message[4] - '0');
-            }
-            Dir = message[1] - '0';
-            //如果已经处于零位，且还是向左转，就不要旋转了，代表已经处于零位。
-//            if(bigStepMotorSwitch_State_PA0 == 0)
-////							|| bigStepMotorSwitch_State_PA1 == 0 || bigStepMotorSwitch_State_PA4 == 0)
-////							&& (Dir == 1))
-//            {
-//                runover = 1;
-//            }
-//            else
-//            {
-                runover = 0;
-                speed_cntr_Move(value, 20, 20, 2000);
-//            }
-
-            okCmd = TRUE;
-            runOver();
-        }
+          okCmd = TRUE;
+          runOver();
+					
+					
+					
+				}
 
         if(message[0] == 'S')		//S1-3旋转1-3号小电机，S0为归位
         {
